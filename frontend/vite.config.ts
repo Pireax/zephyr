4ea68ea057f989e829738b5defc5bfbb973bda01
Pim Remkes <pim.remkes@gmail.com>
2026-08-26 15:48:24 +0200

    Use glb instead of a gltf and bin file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.gltf', '**/*.bin'],
+  assetsInclude: ['**/*.glb'],
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
