a82ac3a3aeb1a199aed2cc47ad5b613f8ca423a0
Pim <pim.remkes@gmail.com>
2026-09-01 16:29:42 +0200

    Improve the rain and add time of day dynamics (#4)
    * Improve the rain and add time of day dynamics
    
    * Decrease darkening from rain

--- frontend/src/assets/rain.vert.glsl
+++ frontend/src/assets/rain.vert.glsl
@@ -4,15 +4,33 @@ uniform float uTime;
 uniform float uSize;
 uniform float uOverallSpeed;
 uniform float uHeight;
+uniform vec3 uPosition;
+uniform vec2 windDirection;
+
+varying float vAngle;
+
+#include <snoise>
 
 void main() {
   vec3 local = position;
-  float wrappedY = mod(local.y - uTime * uOverallSpeed * aSpeed, uHeight);
+  
+  float fallSpeed = uOverallSpeed * aSpeed;
+  float wrappedY = mod(local.y - uTime * fallSpeed, uHeight);
+
+  float windStrength = 0.5;
+  vec3 n = snoiseD(windDirection * uTime - uPosition.xz / 50.0);
+  float windNoise = 1.0 - n.x;
+  vec2 windDerivative = n.yz;
+  vec2 windOffset = windDirection * windNoise * windStrength;
+
   vec3 worldPos = vec3(
-    local.x,
+    local.x + windOffset.x,
     wrappedY - 3.0,
-    local.z
+    local.z + windOffset.y
   );
+
+  float lateralSpeed = dot(windDerivative, windDirection) * windStrength;
+  vAngle = atan(lateralSpeed, fallSpeed);
 
   vec4 mvPosition = modelViewMatrix * vec4(worldPos, 1.0);
   gl_Position = projectionMatrix * mvPosition;
